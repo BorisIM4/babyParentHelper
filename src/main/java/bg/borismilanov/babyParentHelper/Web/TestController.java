3829380b500c4bde3393cc59fc13446ba0ab43e1
JRB <jrb@abv.bg>
2023-03-24 23:13:34 +0200

    Add test files, for starting project

---
+++ src/main/java/bg/borismilanov/babyParentHelper/Web/TestController.java
@@ -0,0 +1,13 @@
+package bg.borismilanov.babyParentHelper.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String testPage(){
+        return "test";
+    }
+}
